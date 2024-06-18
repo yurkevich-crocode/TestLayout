ac948d2e583444622c6b313aecc3d886005f227b
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2024-06-18 20:00:10 +0300

    done

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,35 +1,51 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import style from "./ContactUs.module.scss";
 import Button from "@/components/Button";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const ContactUs = () => {
-  const [animationStarted, setAnimationStarted] = useState(false);
+  const backgroundControls = useAnimation();
+  const textControls = useAnimation();
 
   useEffect(() => {
+    const sequence = async () => {
+      await backgroundControls.start({
+        opacity: 1,
+        backgroundColor: "black",
+        width: ["49px", "196px"],
+        transition: { duration: 1, ease: "easeInOut" },
+      });
+
+      await textControls.start({
+        opacity: 1,
+        color: "white",
+        transition: { delay: 0.25, duration: 0.75, ease: "easeInOut" },
+      });
+    };
+
     const timer = setTimeout(() => {
-      setAnimationStarted(true);
-    }, 100);
+      sequence();
+    }, 500);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [backgroundControls, textControls]);
+
   return (
     <motion.div
       className={style["contact-us-btn"]}
-      initial={{ opacity: 0, y: 0, width: "max-content" }}
-      animate={
-        animationStarted ? { opacity: 1, y: 0, width: "max-content" } : {}
-      }
+      initial={{ opacity: 1, backgroundColor: "black", width: "49px" }}
+      animate={backgroundControls}
     >
       <Button svg={"/icons/mobile.svg"} rounded={true} />
       <motion.span
-        initial={{ opacity: 0, y: 0, width: "max-content" }}
-        animate={
-          animationStarted ? { opacity: 1, y: 0, width: "max-content" } : {}
-        }
-        transition={{ duration: 0.75, ease: "easeInOut" }}
+        initial={{ opacity: 0 }}
+        animate={textControls}
+        transition={{
+          opacity: { duration: 0 },
+          color: { delay: 0.25, duration: 0.75, ease: "easeInOut" },
+        }}
       >
         Contact Us Now
       </motion.span>
